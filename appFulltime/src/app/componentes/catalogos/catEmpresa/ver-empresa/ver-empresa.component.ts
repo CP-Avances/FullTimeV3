a42f604a3cefa8f0c13b239fbc11ad10aa81e9a7
Jhonatan <pasantesistemas.cp@gmail.com>
2023-03-01 13:27:16 -0500

    Actualizacion SistemaWeb Fulltime4,0 01 de marzo del 2023

--- appFulltime/src/app/componentes/catalogos/catEmpresa/ver-empresa/ver-empresa.component.ts
+++ appFulltime/src/app/componentes/catalogos/catEmpresa/ver-empresa/ver-empresa.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { MatDialog } from '@angular/material/dialog';
 import { PageEvent } from '@angular/material/paginator';
 import { Router } from '@angular/router';
+import * as moment from 'moment';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 import { RegistrarSucursalesComponent } from 'src/app/componentes/catalogos/catSucursal/registrar-sucursales/registrar-sucursales.component';
 import { EditarSucursalComponent } from 'src/app/componentes/catalogos/catSucursal/editar-sucursal/editar-sucursal.component';
@@ -12,7 +17,11 @@ import { MetodosComponent } from 'src/app/componentes/administracionGeneral/meto
 import { LogosComponent } from 'src/app/componentes/catalogos/catEmpresa/logos/logos.component';
 
 import { SucursalService } from 'src/app/servicios/sucursales/sucursal.service';
-import { EmpresaService } from 'src/app/servicios/catalogos/catEmpresa/empresa.service'
+import { EmpresaService } from 'src/app/servicios/catalogos/catEmpresa/empresa.service';
+
+import { ProgressSpinnerMode } from '@angular/material/progress-spinner';
+import { ThemePalette } from '@angular/material/core';
+import { EmpleadoService } from 'src/app/servicios/empleado/empleadoRegistro/empleado.service';
 
 @Component({
   selector: 'app-ver-empresa',
@@ -37,21 +46,51 @@ export class VerEmpresaComponent implements OnInit {
   conCambios: boolean = true;
   cambiosTodos: boolean = true;
 
+  // DATOS DE FORMULARIO DE COLORES
+  principal = new FormControl('');
+  secundario = new FormControl('');
+
+  public coloresForm = new FormGroup({
+    color_p: this.principal,
+    color_s: this.secundario
+  });
+
+  idEmpleado: number;
+  public empleado: any = [];
+  p_color: any;
+  s_color: any;
+  frase: any;
+
+  verColores: boolean = false;
+  verFrase: boolean = false;
+
+  /**
+   * VARIABLES PROGRESS SPINNER
+   */
+  color: ThemePalette = 'primary';
+  mode: ProgressSpinnerMode = 'indeterminate';
+  value = 10;
+  habilitarprogress: boolean = false;
+
   constructor(
     public ventana: MatDialog,
     public empresa: EmpresaService,
     public router: Router,
     public restS: SucursalService,
+    public restE: EmpleadoService,
     private toastr: ToastrService,
   ) {
     var cadena = this.router.url;
     var aux = cadena.split("/");
     this.idEmpresa = aux[2];
+    this.idEmpleado = parseInt(localStorage.getItem('empleado'));
+    console.log("Id empleado: ",this.idEmpleado);
   }
 
   ngOnInit(): void {
     this.CargarDatosEmpresa();
     this.ObtenerSucursal();
+    this.ObtenerEmpleados(this.idEmpleado);
   }
 
   // METODO DE CONTROL DE PAGINACION
@@ -66,15 +105,17 @@ export class VerEmpresaComponent implements OnInit {
     this.datosEmpresa = [];
     this.empresa.ConsultarDatosEmpresa(parseInt(this.idEmpresa)).subscribe(datos => {
       this.datosEmpresa = datos;
+      this.p_color = this.datosEmpresa[0].color_p;
+      this.s_color = this.datosEmpresa[0].color_s;
       if (this.datosEmpresa[0].establecimiento === null || this.datosEmpresa[0].establecimiento === '' || this.datosEmpresa[0].establecimiento === undefined) {
         this.nombre_establecimiento = 'establecimientos';
       }
       else {
         this.nombre_establecimiento = this.datosEmpresa[0].establecimiento;
       }
-
-      this.ObtenerLogotipo();
-
+      if (this.datosEmpresa[0].logo != null) {
+        this.ObtenerLogotipo();
+      }
       if (this.datosEmpresa[0].cambios === true) {
         if (this.datosEmpresa[0].dias_cambio === 0) {
           this.cambiosTodos = false;
@@ -122,7 +163,7 @@ export class VerEmpresaComponent implements OnInit {
 
   // VENTANA DE EDICION DE ESTABLECIMIENTOS
   AbrirVentanaEditarSucursal(datosSeleccionados: any): void {
-    this.ventana.open(EditarSucursalComponent, { width: '900px', data: datosSeleccionados })
+    this.ventana.open(EditarSucursalComponent, { width: '650px', data: datosSeleccionados })
       .afterClosed().subscribe((items: any) => {
         if (items.actualizar === true) {
           this.ObtenerSucursal();
@@ -132,22 +173,20 @@ export class VerEmpresaComponent implements OnInit {
 
   // VENTANA DE REGISTRO DE ESTABLECIMIENTO
   AbrirVentanaRegistrarSucursal() {
-    this.ventana.open(RegistrarSucursalesComponent,
-      { width: '900px', data: parseInt(this.idEmpresa) })
+    this.ventana.open(RegistrarSucursalesComponent, { width: '650px', data: parseInt(this.idEmpresa) })
       .afterClosed().subscribe((items: any) => {
-        this.ObtenerSucursal();
+          this.ObtenerSucursal();
       });
   }
 
   // VENTANA PARA REVISAR FORMATO DE REPORTES COLORES
   AbrirVentanaReportes(datos_empresa: any, ventana: any) {
-    this.ventana.open(ColoresEmpresaComponent, {
-      width: '700',
-      data: { datos: datos_empresa, ventana: ventana }
-    })
+    this.ventana.open(ColoresEmpresaComponent, {width: '400px',data: { datos: datos_empresa, ventana: ventana }})
       .afterClosed().subscribe((items: any) => {
         if (items) {
           if (items.actualizar === true) {
+            this.ObtenerSucursal();
+            this.ObtenerLogotipo();
             this.CargarDatosEmpresa();
           }
         }
@@ -156,9 +195,7 @@ export class VerEmpresaComponent implements OnInit {
 
   // METODO PARA EDITAR LOGO DE EMPRESA
   EditarLogo() {
-    this.ventana.open(LogosComponent, {
-      width: '500px',
-      data: { empresa: parseInt(this.idEmpresa), pagina: 'empresa' }
+    this.ventana.open(LogosComponent, { width: '500px', data: { empresa: parseInt(this.idEmpresa), pagina: 'empresa' }
     }).afterClosed()
       .subscribe((res: any) => {
         this.ObtenerLogotipo();
@@ -189,14 +226,147 @@ export class VerEmpresaComponent implements OnInit {
 
   // VENTANA DE REGISTRO DE FRASE DE SEGURIDAD
   AbrirVentanaSeguridad(datosSeleccionados: any) {
-    this.ventana.open(TipoSeguridadComponent, { width: '500', data: datosSeleccionados })
+    this.ventana.open(TipoSeguridadComponent, { width: '350px', data: datosSeleccionados })
       .afterClosed().subscribe((items: any) => {
-        if (items) {
-          if (items === true) {
-            this.CargarDatosEmpresa();
-          }
-        }
+        this.ObtenerSucursal();
+        this.ObtenerLogotipo();
+        this.CargarDatosEmpresa();
       });
+  }
+
+  // METODO DE REGISTRO DE COLORES
+  CambiarColores() {
+    this.habilitarprogress = true;
+    let datos = {
+      color_p: this.p_color,
+      color_s: this.s_color,
+      id: this.datosEmpresa[0].id
+    }
+    this.empresa.ActualizarColores(datos).subscribe(data => {
+      this.toastr.success('Colores de encabezados de reportes registrados.', '', {
+        timeOut: 6000,
+      });
+      this.obtenerColores();
+      this.habilitarprogress = false;
+    })
+  }
+
+  // METODO PARA OBTENER DATOS DE EMPRESA COLORES - MARCA DE AGUA
+  public empresas: any = [];
+  obtenerColores() {
+    this.empresas = [];
+    this.empresa.ConsultarDatosEmpresa(this.datosEmpresa[0].id).subscribe(res => {
+      this.empresas = res;
+      this.p_color = this.empresas[0].color_p;
+      this.s_color = this.empresas[0].color_s;
+      this.frase = this.empresas[0].marca_agua;
+    });
+  }
+
+  // METODO PARA VER LA INFORMACIÓN DEL EMPLEADO 
+  ObtenerEmpleados(idemploy: any) {
+    this.empleado = [];
+    this.restE.BuscarUnEmpleado(idemploy).subscribe(data => {
+      this.empleado = data;
+    })
+  }
+
+  /** ************************************************************************************************** ** 
+   ** **                                 METODO PARA EXPORTAR A PDF                                   ** **
+   ** ************************************************************************************************** **/
+
+   // GENERACION DE REPORTE DE PDF
+   GenerarPdf(action = 'open') {
+    const documentDefinition = this.getDocumentDefinicion();
+    pdfMake.createPdf(documentDefinition).open();
+  }
+
+  // DEFINICION DE PDF CABECERA - PIE DE PAGINA - ESTRUCTURA DE REPORTE
+  getDocumentDefinicion() {
+    sessionStorage.setItem('Empresas', this.empresas);
+
+    console.log("Empleado Nombre: ",this.empleado[0].nombre);
+    return {
+
+      // ENCABEZADO DE LA PÁGINA
+      pageOrientation: 'landscape',
+      watermark: { text: this.frase, color: 'blue', opacity: 0.1, bold: true, italics: false },
+      header: { text: 'Impreso por:  ' + this.empleado[0].nombre + ' ' + this.empleado[0].apellido, margin: 5, fontSize: 9, opacity: 0.3, alignment: 'right' },
+
+      // PIE DE PÁGINA
+      footer: function (currentPage: any, pageCount: any, fecha: any, hora: any) {
+        var f = moment();
+        fecha = f.format('YYYY-MM-DD');
+        hora = f.format('HH:mm:ss');
+        return {
+          margin: 10,
+          columns: [
+            'Fecha: ' + fecha + ' Hora: ' + hora,
+            {
+              text: [
+                {
+                  text: '© Pag ' + currentPage.toString() + ' of ' + pageCount,
+                  alignment: 'right', color: 'blue', opacity: 0.5
+                }
+              ],
+            }
+          ], fontSize: 10, color: '#A4B8FF',
+        }
+      },
+      content: [
+        { image: this.logo, width: 150, margin: [10, -25, 0, 5] },
+        { text: 'FORMATO REPORTES', bold: true, fontSize: 20, alignment: 'center', margin: [0, -30, 0, 10] },
+        this.PresentarDataPDFEmpresas(),
+      ],
+      styles: {
+        tableHeader: { fontSize: 12, bold: true, alignment: 'center', fillColor: this.p_color },
+        tableHeaderS: { fontSize: 12, bold: true, alignment: 'center', fillColor: this.s_color },
+        itemsTable: { fontSize: 10 },
+        itemsTableC: { fontSize: 10, alignment: 'center' },
+      }
+    };
+  }
+
+  // ESTRUCTURA DE PDF
+  PresentarDataPDFEmpresas() {
+    return {
+      columns: [
+        { width: '*', text: '' },
+        {
+          width: 'auto',
+          table: {
+            widths: ['auto', '*', 'auto', '*', 'auto', 'auto', 'auto', '*', 'auto'],
+            body: [
+              [
+                { text: 'ID', style: 'tableHeader' },
+                { text: 'NOMBRE', style: 'tableHeader' },
+                { text: 'RUC', style: 'tableHeader' },
+                { text: 'DIRECCIÓN', style: 'tableHeader' },
+                { text: 'TELÉFONO', style: 'tableHeader' },
+                { text: 'EMAIL', style: 'tableHeader' },
+                { text: 'TIPO EMPRESA', style: 'tableHeader' },
+                { text: 'REPRESENTANTE', style: 'tableHeader' },
+                { text: 'RESUMEN', style: 'tableHeaderS' }
+              ],
+              ...this.datosEmpresa.map(obj => {
+                return [
+                  { text: obj.id, style: 'itemsTableC' },
+                  { text: obj.nombre, style: 'itemsTable' },
+                  { text: obj.ruc, style: 'itemsTableC' },
+                  { text: obj.direccion, style: 'itemsTable' },
+                  { text: obj.telefono, style: 'itemsTableC' },
+                  { text: obj.correo_empresa, style: 'itemsTable' },
+                  { text: obj.tipo_empresa, style: 'itemsTable' },
+                  { text: obj.representante, style: 'itemsTable' },
+                  { text: 'Generalidades', style: 'itemsTable' },
+                ];
+              })
+            ]
+          }
+        },
+        { width: '*', text: '' },
+      ]
+    };
   }
 
 }
